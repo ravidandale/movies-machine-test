33aacb921232cd16d12640638b71969a7d965d9e
Ravi Dandale <dandaleravi@gmail.com>
2024-05-23 17:49:27 +0530

    Brief description of your changes

--- src/components/CastList.js
+++ src/components/CastList.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const CastList = ({ cast }) => {
-    return (
-        <div>
-            <h2>Cast </h2>
-            <ul>
-                {cast.map((member) => (
-                    <li key={member.cast_id}>{member.name} as {member.character}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="cast-list">
+      <h3>Cast</h3>
+      <ul>
+        {cast.map((member) => (
+          <li key={member.cast_id} className="cast-member">
+            <img
+              src={member.profile_path ? `https://image.tmdb.org/t/p/w200${member.profile_path}` : 'https://via.placeholder.com/200x300'}
+              alt={member.name}
+              className="cast-member-photo"
+            />
+            <div className="cast-member-info">
+              <p><strong>{member.name}</strong></p>
+              <p>{member.character}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CastList;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,35 +2,54 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieDetail, fetchMovieCast } from '../api';
 import CastList from '../components/CastList';
-import MovieInfo from '../components/MovieInfo';
+import '../index.css'; 
 
 const MovieDetail = () => {
-    const { id } = useParams();
-    const [movie, setMovie] = useState(null);
-    const [cast, setCast] = useState([]);
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [cast, setCast] = useState([]);
 
-    useEffect(() => {
-        const loadMovieDetail = async () => {
-            const movieData = await fetchMovieDetail(id);
-            if (movieData) {
-                setMovie(movieData);
-            }
-            const castData = await fetchMovieCast(id);
-            if (castData) {
-                setCast(castData.cast);
-            }
-        };
-        loadMovieDetail();
-    }, [id]);
+  useEffect(() => {
+    const loadMovieDetail = async () => {
+      const data = await fetchMovieDetail(id);
+      if (data) {
+        setMovie(data);
+      }
+    };
 
-    if (!movie) return <div>Loading..</div>;
+    const loadMovieCast = async () => {
+      const data = await fetchMovieCast(id);
+      if (data) {
+        setCast(data.cast);
+      }
+    };
 
-    return (
-        <div>
-            <MovieInfo movie={movie} />
-            <CastList cast={cast} />
+    loadMovieDetail();
+    loadMovieCast();
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;
+
+  return (
+    <div className="movie-detail-container">
+      <div className="movie-info">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="movie-detail-poster"
+        />
+        <div className="movie-detail-content">
+          <h2>{movie.title}</h2>
+          
+          <p><strong>Release Date:</strong> {movie.release_date}</p>
+          <p><strong>Rating:</strong> {movie.vote_average}/10</p>
+          <p><strong>Genres:</strong> {movie.genres.map(genre => genre.name).join(', ')}</p>
+          <p><strong>Overview:</strong>{movie.overview}</p>
         </div>
-    );
+      </div>
+      <CastList cast={cast} />
+    </div>
+  );
 };
 
 export default MovieDetail;
